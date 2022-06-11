3016e874c19c3fb0b062465c559e79d6e00350f1
rahul-adpushup <rahul.kumar@adpushup.com>
2022-06-11 17:34:49 +0000

    made changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-
+const PORT = 5000;
 app.get("/",(_,res)=>res.send("Hello Our Mode on"));
 
-app.listen(process.env.PORT || 5000,()=>console.log("Server is running..."))
+app.listen(process.env.PORT || PORT,()=>console.log("Server is running..."+PORT))
